b6285dcc4e097ada6062a246227ba090b375ec10
Alexandre Aubin <alex.aubin@mailoo.org>
2017-07-22 20:09:49 -0400

    Disabling error display

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 // FOR DEBUGGING, NOT FOR PROD !!
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
+// ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
 
 $simone_root = "https://some.domaine.tld/simone/";
 $email_from  = "yunobot@some.domaine.tld";
